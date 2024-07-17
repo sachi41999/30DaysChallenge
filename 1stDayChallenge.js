f1bc0340c985299b32134fcbfde2ebc7319a828e
sachi41999 <sachisabya41999@gmail.com>
2024-07-17 22:23:28 +0530

    1st challenge complete

--- 1stDayChallenge.js
+++ 1stDayChallenge.js
@@ -1,1 +1,27 @@
 console.log('hello');
+var x1=23;
+let x2=21;
+
+console.log(x1,' ',x2);
+
+const x3=true;
+console.log(x3);
+
+var a_1=12;
+let b_1='star';
+const c_1=false;
+let obj={name:"sabya",age:23}
+let arr=['sab',10];
+
+console.log(`a_1:${typeof a_1}, b_1:${typeof b_1}, c_1:${typeof c_1}, obj:${typeof obj}, arr:${typeof arr}`);
+
+let var_1=83;
+console.log(`var_1: ${var_1}`);
+var_1='st';
+console.log(`var_1: ${var_1}`);
+
+const con_1="const value";
+con_1="again value assign";
+
+console.log('error -> ',con_1);
+
